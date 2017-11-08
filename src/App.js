36c39fb1fbe13bccc80c47840863b32a640c925f
cundejo <cundejo85@gmail.com>
2017-11-07 23:07:22 -0300

    starting a new react app.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,6 @@
 import React from 'react';
 
-const Headline = () => {
-    return (
-        <h1 className="title">Hello World</h1>
-    )
-};
-
-const Greeting = (props) => {
-    const {name, age} = props;
-    return (
-        <p>Hello {name}, you have {age}, and we are testing react.</p>
-    )
-};
-
-const App = () => {
-    return (
-        <div>
-            <Headline/>
-            <Greeting name="Oliver" age={25}/>
-        </div>
-    )
-};
-
-Greeting.propTypes = {
-    name: React.PropTypes.string,
-    age: React.PropTypes.number.isRequired,
-};
-
+const App = () => <h1>Hello World!</h1>
 
 export default App;
+
