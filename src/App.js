618c9691df350dffe86be0b741cd9dff2497a9d1
cundejo <cundejo85@gmail.com>
2017-11-08 00:16:10 -0300

    some simply examples so far

--- src/App.js
+++ src/App.js
@@ -1,6 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const App = () => <h1>Hello World!</h1>
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            text: "Default text"
+        }
+    }
+
+    updateText(event) {
+        this.setState({ text: event.target.value });
+    }
+
+    render() {
+        const { name, age } = this.props;
+        return (
+            <div>
+                <h1>Hello {name}, you are {age} years old.</h1>
+                <p>Enter something in the input and see what happens</p>
+                <Widget change={this.updateText.bind(this)} />
+                <p><code>state.text</code>: {this.state.text}</p>
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    name: "Default name",
+    age: 99
+}
+
+const Widget = (props) => (
+    <input type="text" onChange={props.change} />
+);
 
 export default App;
-
